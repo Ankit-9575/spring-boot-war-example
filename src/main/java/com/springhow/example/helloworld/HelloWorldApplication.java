481ec9abe97297e1357506fbc9cb05f800d98eb4
Ankit-9575 <95208463+Ankit-9575@users.noreply.github.com>
2021-12-26 14:39:59 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "this is my first pipeline fully automated";
+        return "hi guys";
     }
 
 }
