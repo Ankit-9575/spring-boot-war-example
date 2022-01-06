7d3d5667fb5a2791c72c14a1c4535febb96bbd7d
Ankit-9575 <95208463+Ankit-9575@users.noreply.github.com>
2022-01-06 14:10:46 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "hi there what is happening........!!";
+        return "hi there what's happening...........!!";
     }
 
 }
